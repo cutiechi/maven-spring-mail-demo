7eee3d25e0728ce2c9c02a84fa3092ce720eff62
cutiechi <chi@cutie.ooo>
2018-09-15 14:31:09 +0800

    feat(config): add mail config class

---
+++ src/main/java/com/cutiechi/demo/config/MailConfig.java
@@ -0,0 +1,13 @@
+package com.cutiechi.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 邮件配置类
+ *
+ * @author Cutie Chi
+ */
+@Configuration
+public class MailConfig {
+
+}
